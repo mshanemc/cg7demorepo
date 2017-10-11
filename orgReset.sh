1c2e30905864a1f58ac3ac194b73a86cff91533b
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-10-11 15:19:31 -0500

    reset includes git

--- orgReset.sh
+++ orgReset.sh
@@ -4,3 +4,8 @@ cp -f backup/* force-app/main/default/flexipages
 
 # put that pack into the Salesforce org
 sfdx force:source:push -f
+
+#check in the changes so the source stays clean
+git add force-app/main/default/flexipages/Merchandising_Mix_Record_Page.flexipage-meta.xml
+git commit -m 'auto-reset'
+git push origin master
